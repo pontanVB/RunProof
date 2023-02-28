58ebcc67628993b45015eb9fb3cab26a7d7355bb
Carolina Rönnewall <carolina.ronnewall@gmail.com>
2023-02-28 16:38:38 +0100

    drawer and some pub adds

--- runproof/lib/widgets/drawer_widget.dart
+++ runproof/lib/widgets/drawer_widget.dart
@@ -1,239 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:gbg_varvet/utils/utils.dart';
 import 'package:provider/provider.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:gbg_varvet/utils/db_functions.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key, required this.title});
 
   final String title;
 
+  Future<void> _showMyDialog(context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Du är påväg att loggas ut'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: const <Widget>[
+                Text('Är du säker på att du vill logga ut?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+                child: const Text('Confirm'),
+                onPressed: () {
+                  signUserOut();
+                  Navigator.of(context).pop();
+                }),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      child: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: Padding(
-          padding: const EdgeInsets.all(0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                height: 250,
-                child: DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Color(0xFF1F4A7B),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Image.asset(
-                          'assets/images/runprooflogo.png',
-                        ),
-                      ),
-                      const Expanded(
-                        flex: 6,
-                        child: Text(
-                          '  RunProof',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Color(0xFF94B0DA),
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(3, 3),
-                                blurRadius: 3,
-                                color: Colors.black54,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 220,
+            child: DrawerHeader(
+              padding: const EdgeInsets.all(0),
+              decoration: const BoxDecoration(
+                color: Color(0xFF1F4A7B),
               ),
-              Expanded(child: _PatientsList()),
-            ],
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Image.asset(
+                      'assets/images/runprooflogo.png',
+                    ),
+                  ),
+                  const Expanded(
+                    flex: 6,
+                    child: Text(
+                      '  RunProof',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Color(0xFF94B0DA),
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(3, 3),
+                            blurRadius: 3,
+                            color: Colors.black54,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          // child: Column(
-          //   // Important: Remove any padding from the ListView.
-          //   children: [
-          //     SizedBox(
-          //       height: 250,
-          //       child: DrawerHeader(
-          //         decoration: const BoxDecoration(
-          //           color: Color(0xFF1F4A7B),
-          //         ),
-          //         child: Row(
-          //           children: [
-          //             Expanded(
-          //               flex: 4,
-          //               child: Image.asset(
-          //                 'assets/images/runprooflogo.png',
-          //               ),
-          //             ),
-          //             const Expanded(
-          //               flex: 6,
-          //               child: Text(
-          //                 '  RunProof',
-          //                 style: TextStyle(
-          //                   fontSize: 30,
-          //                   color: Color(0xFF94B0DA),
-          //                   shadows: <Shadow>[
-          //                     Shadow(
-          //                       offset: Offset(3, 3),
-          //                       blurRadius: 3,
-          //                       color: Colors.black54,
-          //                     ),
-          //                   ],
-          //                 ),
-          //               ),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ),
-          //     ListTile(
-          //       tileColor: const Color(0xFF94B0DA),
-          //       title: const Text(
-          //         'TÄLT X',
-          //         textScaleFactor: 2,
-          //         textAlign: TextAlign.center,
-          //         style: TextStyle(color: Color(0xFF1F4A7B)),
-          //       ),
-          //       onTap: () {
-          //         // Update the state of the app
-          //         // ...
-          //         // Then close the drawer
-          //         Navigator.pop(context);
-          //       },
-          //     ),
-          //     ListTile(
-          //       leading: const Icon(
-          //         Icons.check_circle,
-          //         color: Color(0xFF75C883),
-          //         size: 30,
-          //       ),
-          //       title: const Text(
-          //         'Ingrid #44844',
-          //         textScaleFactor: 1.5,
-          //         style: TextStyle(color: Color(0xFF06192F)),
-          //       ),
-          //       onTap: () {
-          //         // Update the state of the app
-          //         // ...
-          //         // Then close the drawer
-          //         Navigator.pop(context);
-          //       },
-          //     ),
-          //     ListTile(
-          //       leading: const Icon(
-          //         Icons.star,
-          //         color: Color(0xFFD9D9D9),
-          //         size: 30,
-          //       ),
-          //       title: const Text(
-          //         'HÄLSODEKLARATION',
-          //         textScaleFactor: 1.5,
-          //         style: TextStyle(color: Color(0xFF06192F)),
-          //       ),
-          //       onTap: () {
-          //         // Update the state of the app
-          //         // ...
-          //         // Then close the drawer
-          //         Navigator.pop(context);
-          //       },
-          //     ),
-          //     ListTile(
-          //       leading: const Icon(
-          //         Icons.star,
-          //         color: Color(0xFFD9D9D9),
-          //         size: 30,
-          //       ),
-          //       title: const Text(
-          //         'HÄLSODEKLARATION',
-          //         textScaleFactor: 1.5,
-          //         style: TextStyle(color: Color(0xFF06192F)),
-          //       ),
-          //       onTap: () {
-          //         // Update the state of the app
-          //         // ...
-          //         // Then close the drawer
-          //         Navigator.pop(context);
-          //       },
-          //     ),
-          //     ListTile(
-          //       leading: const Icon(
-          //         Icons.star,
-          //         color: Color(0xFFD9D9D9),
-          //         size: 30,
-          //       ),
-          //       title: const Text(
-          //         'HÄLSODEKLARATION',
-          //         textScaleFactor: 1.5,
-          //         style: TextStyle(color: Color(0xFF06192F)),
-          //       ),
-          //       onTap: () {
-          //         // Update the state of the app
-          //         // ...
-          //         // Then close the drawer
-          //         Navigator.pop(context);
-          //       },
-          //     ),
-          //     SizedBox(
-          //       height: 40,
-          //       width: 280,
-          //       child: TextField(
-          //         textAlign: TextAlign.left,
-          //         style: const TextStyle(
-          //           fontSize: 20,
-          //           color: Colors.black,
-          //         ),
-          //         decoration: InputDecoration(
-          //           prefixIcon: const Icon(Icons.search),
-          //           hintText: "Sök",
-          //           filled: true,
-          //           fillColor: Colors.white,
-          //           border: OutlineInputBorder(
-          //               borderRadius: BorderRadius.circular(15.0),
-          //               borderSide:
-          //                   const BorderSide(width: 2, color: Colors.black)),
-          //         ),
-          //       ),
-          //     ),
-          //     ElevatedButton(
-          //         onPressed: () => print("sign out"),
-          //         style: ElevatedButton.styleFrom(
-          //           backgroundColor:
-          //               const Color(0xFFAB1C48), //background color of button
-          //           elevation: 10, //elevation of button
-          //           shape: RoundedRectangleBorder(
-          //               //to set border radius to button
-          //               borderRadius: BorderRadius.circular(15)),
-          //           padding: const EdgeInsets.only(
-          //               left: 20, right: 20, top: 10, bottom: 10),
-          //         ),
-          //         child: const Text(
-          //           "LOGGA UT",
-          //           style: TextStyle(color: Colors.white, fontSize: 20),
-          //         ))
-
-          //     //ListTile(
-          //     //title: const Text('Item 2'),
-          //     //onTap: () {
-          //     //Navigator.push(
-          //     //context,
-          //     //MaterialPageRoute(builder: (context) => TextRecognitionPage())
-          //     //);
-
-          //     //},
-          //     //),
-          //   ],
-          // ),
-        ),
+          Expanded(child: _PatientsList()
+          ),
+          Center(
+            child: FloatingActionButton.extended(
+              extendedPadding: const EdgeInsets.all(20),
+              onPressed: () => _showMyDialog(context),
+              label: const Text('SIGN OUT'),
+              icon: const Icon(Icons.logout),
+              backgroundColor: const Color(0xFFAB1C48),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -248,57 +113,30 @@ class _PatientsList extends StatelessWidget {
     var patientsList = context.watch<PatientsModel>();
 
     return ListView.builder(
-        itemCount: patientsList.patientsList.length + 1,
+      padding: const EdgeInsets.only(top: 0),
+        itemCount: patientsList.patientsList.length,
         itemBuilder: (context, index) {
-          if (index == patientsList.patientsList.length) {
-            // button
-            return Container(
-                child: ElevatedButton(
-                    onPressed: () => print("sign out"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          const Color(0xFFAB1C48), //background color of button
-                      elevation: 10, //elevation of button
-                      shape: RoundedRectangleBorder(
-                          //to set border radius to button
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.only(
-                          left: 20, right: 20, top: 10, bottom: 10),
-                    ),
-                    child: const Text(
-                      "LOGGA UT",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    )));
-          } else {
             final Map patient = patientsList.getPatient(index);
 
             final name = patient["name"];
             final runningNumber = patient["runningNumber"];
 
-            return ListTile(
-              leading: const Icon(
-                Icons.check_circle,
-                color: Color(0xFF75C883),
-                size: 30,
+            return Card(
+              child: ListTile(
+                leading: CircularPercentIndicator(radius: 20, percent: 0.5, backgroundColor:Colors.black12, progressColor: const Color(0xFF75C883),
+                ),
+                title: Text('$name'.toUpperCase(), textScaleFactor: 1.4
+                ),
+                subtitle: Text('#$runningNumber', textScaleFactor: 1.2,
+                ),
+                trailing: (patientsList.activeIndex == index) ?  const Text('AKTIV', style: TextStyle(color: Color(0xFF75C883), fontWeight: FontWeight.bold)) : null ,
+                onTap: () {
+                  patientsList.setActiveIndex(index);
+                },
               ),
-              title: Text('$name #$runningNumber',
-                  textScaleFactor: 1.5,
-                  style: const TextStyle(color: Color(0xFF06192F))),
-              onTap: () {
-                print(index);
-                patientsList.activeIndex = index;
-              },
             );
           }
-        });
+        );
   }
 }
 
-class PatientListTile extends StatelessWidget {
-  const PatientListTile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
