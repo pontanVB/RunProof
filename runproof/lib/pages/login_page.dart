c0a02b958845a5f80db1a7fcaf5ea15281ccfa34
Philip Dynevik <philip.dynevik@hotmail.com>
2023-03-07 14:14:12 +0100

    fixat varningsmeddelande loginpage

--- runproof/lib/pages/login_page.dart
+++ runproof/lib/pages/login_page.dart
@@ -54,6 +54,72 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               );
             });
+      } else if (error.code == "wrong-password") {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                icon: Icon(Icons.warning_amber_sharp, size: 70),
+                iconColor: Colors.red,
+                title: Text("Oops!"),
+                content: Text('Ange ett giltigt lösenord för att logga in'),
+                actions: [
+                  Center(
+                    child: ElevatedButton(
+                        child: Text("Ok"),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF75C883)),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        }),
+                  )
+                ],
+              );
+            });
+      } else if (usernameController.text.isEmpty) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                icon: Icon(Icons.warning_amber_sharp, size: 70),
+                iconColor: Colors.red,
+                title: Text("Oops!"),
+                content: Text('Ange en giltig email för att logga in'),
+                actions: [
+                  Center(
+                    child: ElevatedButton(
+                        child: Text("Ok"),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF75C883)),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        }),
+                  )
+                ],
+              );
+            });
+      } else if (passwordController.text.isEmpty) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                icon: Icon(Icons.warning_amber_sharp, size: 70),
+                iconColor: Colors.red,
+                title: Text("Oops!"),
+                content: Text('Ange ett giltigt lösenord för att logga in'),
+                actions: [
+                  Center(
+                    child: ElevatedButton(
+                        child: Text("Ok"),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF75C883)),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        }),
+                  )
+                ],
+              );
+            });
       }
     }
     Navigator.of(context).pop();
@@ -61,186 +127,191 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xFF1F4A7B),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 25),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: DropShadowImage(
-                          offset: const Offset(3, 3),
-                          scale: 1,
-                          blurRadius: 5,
-                          borderRadius: 3,
-                          image: Image.asset(
-                            'assets/images/runprooflogo.png',
-                          ),
-                        ),
-                      ),
-                      const Expanded(
-                        flex: 6,
-                        child: Text(
-                          'RunProof',
-                          style: TextStyle(
-                            fontSize: 52,
-                            color: Color(0xFF94B0DA),
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(3, 3),
-                                blurRadius: 3,
-                                color: Colors.black54,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Row(
-                    children: const [
-                      Expanded(child: Spacer()),
-                      Expanded(
-                          flex: 5,
-                          child: Text(
-                            'Användarnamn',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              color: Colors.white,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      onVerticalDragEnd: (DragEndDetails details) =>
+          FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+          backgroundColor: const Color(0xFF1F4A7B),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: DropShadowImage(
+                            offset: const Offset(3, 3),
+                            scale: 1,
+                            blurRadius: 5,
+                            borderRadius: 3,
+                            image: Image.asset(
+                              'assets/images/runprooflogo.png',
                             ),
-                          )),
-                      Expanded(child: Spacer()),
-                    ],
+                          ),
+                        ),
+                        const Expanded(
+                          flex: 6,
+                          child: Text(
+                            'RunProof',
+                            style: TextStyle(
+                              fontSize: 52,
+                              color: Color(0xFF94B0DA),
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(3, 3),
+                                  blurRadius: 3,
+                                  color: Colors.black54,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: Row(
-                    children: [
-                      const Expanded(child: Spacer()),
-                      Expanded(
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      children: const [
+                        Expanded(child: Spacer()),
+                        Expanded(
+                            flex: 5,
+                            child: Text(
+                              'Användarnamn',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                            )),
+                        Expanded(child: Spacer()),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Row(
+                      children: [
+                        const Expanded(child: Spacer()),
+                        Expanded(
+                            flex: 5,
+                            child: TextField(
+                              textAlign: TextAlign.left,
+                              controller: usernameController,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: "e-mail",
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                              ),
+                            )),
+                        const Expanded(child: Spacer()),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      children: const [
+                        Expanded(child: Spacer()),
+                        Expanded(
+                            flex: 5,
+                            child: Text(
+                              'Lösenord',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                            )),
+                        Expanded(child: Spacer()),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Row(
+                      children: [
+                        const Expanded(child: Spacer()),
+                        Expanded(
                           flex: 5,
                           child: TextField(
                             textAlign: TextAlign.left,
-                            controller: usernameController,
+                            controller: passwordController,
                             style: const TextStyle(
                               fontSize: 20,
                               color: Colors.black,
                             ),
                             decoration: InputDecoration(
-                              hintText: "e-mail",
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                            ),
-                          )),
-                      const Expanded(child: Spacer()),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Row(
-                    children: const [
-                      Expanded(child: Spacer()),
-                      Expanded(
-                          flex: 5,
-                          child: Text(
-                            'Lösenord',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              color: Colors.white,
-                            ),
-                          )),
-                      Expanded(child: Spacer()),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: Row(
-                    children: [
-                      const Expanded(child: Spacer()),
-                      Expanded(
-                        flex: 5,
-                        child: TextField(
-                          textAlign: TextAlign.left,
-                          controller: passwordController,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
+                                hintText: "* * * * *",
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                suffix: InkWell(
+                                  onTap: _togglePasswordView,
+                                  child: Icon(
+                                    _isHidden
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                )),
+                            obscureText: _isHidden,
+                            enableSuggestions: false,
+                            autocorrect: false,
                           ),
-                          decoration: InputDecoration(
-                              hintText: "* * * * *",
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        const Expanded(child: Spacer())
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Row(
+                      children: [
+                        const Expanded(child: Spacer()),
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                              onPressed: signUserIn,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(
+                                    0xFF75C883), //background color of button
+                                elevation: 10, //elevation of button
+                                shape: RoundedRectangleBorder(
+                                    //to set border radius to button
+                                    borderRadius: BorderRadius.circular(15)),
+                                padding: const EdgeInsets.all(10),
                               ),
-                              suffix: InkWell(
-                                onTap: _togglePasswordView,
-                                child: Icon(
-                                  _isHidden
-                                      ? Icons.visibility_off
-                                      : Icons.visibility,
+                              child: const Text(
+                                'LOGGA IN',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
                                 ),
                               )),
-                          obscureText: _isHidden,
-                          enableSuggestions: false,
-                          autocorrect: false,
                         ),
-                      ),
-                      const Expanded(child: Spacer())
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: Row(
-                    children: [
-                      const Expanded(child: Spacer()),
-                      Expanded(
-                        flex: 1,
-                        child: ElevatedButton(
-                            onPressed: signUserIn,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(
-                                  0xFF75C883), //background color of button
-                              elevation: 10, //elevation of button
-                              shape: RoundedRectangleBorder(
-                                  //to set border radius to button
-                                  borderRadius: BorderRadius.circular(15)),
-                              padding: const EdgeInsets.all(10),
-                            ),
-                            child: const Text(
-                              'LOGGA IN',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            )),
-                      ),
-                      const Expanded(child: Spacer())
-                    ],
-                  ),
-                )
-              ]),
-        ));
+                        const Expanded(child: Spacer())
+                      ],
+                    ),
+                  )
+                ]),
+          )),
+    );
   }
 
   void _togglePasswordView() {
