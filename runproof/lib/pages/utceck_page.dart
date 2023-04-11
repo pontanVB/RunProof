05abf9a7854a7489552d6224ca7f638065c8429d
Rikard radovac <rikard.radovac@gmail.com>
2023-04-11 13:29:56 +0200

    fix bug with database when overwriting type

--- runproof/lib/pages/utceck_page.dart
+++ runproof/lib/pages/utceck_page.dart
@@ -51,7 +51,7 @@ class _UtcheckPageState extends State<UtcheckPage> {
 
     final String datetime = patient.containsKey("startTime")
         ? patient["startTime"]
-        : '${DateTime.now().hour} :${DateTime.now().minute}';
+        : '${DateTime.now().hour}:${DateTime.now().minute}';
 
     final TextEditingController datetimeController =
         TextEditingController(text: datetime);
@@ -244,6 +244,7 @@ class _UtcheckPageState extends State<UtcheckPage> {
                                     controller: checkComment,
                                     minLines: 4,
                                     maxLines: 6,
+                                    textInputAction: TextInputAction.done,
                                     keyboardType: TextInputType.multiline,
                                     decoration: InputDecoration(
                                         filled: true,
@@ -277,9 +278,12 @@ class _UtcheckPageState extends State<UtcheckPage> {
                               onPressed: () => {
                                 if (_formKey.currentState!.validate())
                                   {
-                                    sendToDatabase(patient, "4"),
+                                    _whatToSend(patient),
                                     Navigator.of(context)
-                                        .popUntil((route) => route.isFirst)
+                                        .popUntil((route) => route.isFirst),
+                                    print(patientsModel.activeIndex),
+                                    patientsModel.removePatient(
+                                        patientsModel.activeIndex),
                                   }
                               },
                               style: ElevatedButton.styleFrom(
@@ -294,4 +298,14 @@ class _UtcheckPageState extends State<UtcheckPage> {
               ))),
     );
   }
+}
+
+void _whatToSend(Map patient) {
+  // helper function for removing uncessesary information when sending to databse
+  if (patient["type"] == "injury") {
+    patient.remove("sickness");
+  } else if (patient["type"] == "sickness") {
+    patient.remove("injury");
+  }
+  sendToDatabase(patient, "4");
 }
