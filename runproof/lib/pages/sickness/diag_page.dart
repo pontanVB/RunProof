f1718da289d0a1cf716af2baaf9d4df1d34241e2
heden1 <Antonheden19@gmail.com>
2023-04-11 13:39:05 +0200

    Makes diagnos page resposive to diffrent sizes of phones

--- runproof/lib/pages/sickness/diag_page.dart
+++ runproof/lib/pages/sickness/diag_page.dart
@@ -150,8 +150,8 @@ class _DiagPageState extends State<DiagPage> {
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
                       child: SizedBox(
-                        width: 350,
-                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.width * 0.26,
                         child: ElevatedButton(
                           onPressed: () {
                             setState(
@@ -181,8 +181,8 @@ class _DiagPageState extends State<DiagPage> {
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
                       child: SizedBox(
-                        width: 350,
-                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.width * 0.26,
                         child: ElevatedButton(
                           onPressed: () {
                             setState(
@@ -213,8 +213,8 @@ class _DiagPageState extends State<DiagPage> {
                         top: 8.0, left: 8.0, right: 8.0, bottom: 15),
                     child: Center(
                       child: SizedBox(
-                        width: 350,
-                        height: 100,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.width * 0.26,
                         child: ElevatedButton(
                           onPressed: () {
                             setState(
@@ -241,7 +241,7 @@ class _DiagPageState extends State<DiagPage> {
                     ),
                   ),
                   Padding(
-                      padding: EdgeInsets.only(top: 10, left: 30),
+                      padding: EdgeInsets.only(top: 5, left: 30),
                       child: Text('ÖVRIGT:',
                           style: TextStyle(color: Colors.black, fontSize: 18))),
                   Center(
@@ -268,41 +268,47 @@ class _DiagPageState extends State<DiagPage> {
                                             borderRadius: BorderRadius.all(
                                                 Radius.circular(20))))),
                               ]))),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 80, top: 10),
-                        child: SizedBox(
-                          width: 100,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20, bottom: 0, left: 15, right: 15),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 7,
                           child: ElevatedButton(
-                              onPressed: () => Navigator.pop(context),
-                              style: ElevatedButton.styleFrom(
-                                  primary: Color.fromARGB(255, 165, 39, 75),
-                                  onPrimary: Colors.white),
-                              child: const Text("TILLBAKA")),
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(left: 80, top: 10),
-                          child: SizedBox(
-                            width: 100,
-                            child: ElevatedButton(
-                              onPressed: () => {
-                                if (_formKey.currentState!.validate())
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const UtcheckPage()))
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.green,
-                                  onPrimary: Colors.white),
-                              child: const Text("NÄSTA"),
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text("TILLBAKA"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 163, 28, 71),
+                              padding: EdgeInsets.symmetric(
+                                  vertical:
+                                      MediaQuery.of(context).size.width * 0.05),
                             ),
-                          )),
-                    ],
-                  )
+                          ),
+                        ),
+                        Spacer(flex: 1),
+                        Expanded(
+                          flex: 7,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const UtcheckPage()));
+                            },
+                            child: const Text("NÄSTA"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              padding: EdgeInsets.symmetric(
+                                  vertical:
+                                      MediaQuery.of(context).size.width * 0.05),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ))),
     );
